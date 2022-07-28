80b3e77bf618161ecbb416e76a2c824f1e139799
js3g20 <js3g20@soton.ac.uk>
2022-07-28 21:34:59 +0100

    Add Com States

--- comms.h
+++ comms.h
@@ -2,14 +2,19 @@
 
 
 // flow values
-#define TRANSMIT    1
-#define RECEIVE     0
+#define TRANSMIT        1
+#define RECEIVE         0
 
 
 // communication status
-#define STATE       0           // state [0:3]
-#define FLOW1       4           // flow 1 [4]
-#define FLOW2       5           // flow 2 [5]
+#define STATE           0           // state [0:3]
+#define FLOW1           4           // flow 1 [4]
+#define FLOW2           5           // flow 2 [5]
 
 // preset states
-#define RESET_STATE 0
+#define RESET_STATE     0
+#define COM_START       4           // green states
+#define COM_END         8
+#define COM_MID         12
+#define COM_TIMEOUT     1           // red states
+#define COM_UNDEFINED   2
