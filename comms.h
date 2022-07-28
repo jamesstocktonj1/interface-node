034dedc094955278c3ead2cf8a5c119d6bc1204d
js3g20 <js3g20@soton.ac.uk>
2022-07-28 20:34:18 +0100

    Create comms

---
+++ comms.h
@@ -0,0 +1,15 @@
+// communication definition file
+
+
+// flow values
+#define TRANSMIT    1
+#define RECEIVE     0
+
+
+// communication status
+#define STATE       0           // state [0:3]
+#define FLOW1       4           // flow 1 [4]
+#define FLOW2       5           // flow 2 [5]
+
+// preset states
+#define RESET_STATE 0
